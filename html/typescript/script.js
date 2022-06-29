f19b0b35d082fe071500e908b3ea12075af0ac90
erica-francine <ericafrancine7@live.com>
2022-06-29 17:55:08 -0300

    novo projeto typescript

---
+++ html/typescript/script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById('button')
+let input1 = document.getElementById('input1')
+let input2 = document.getElementById('input2')
+
+function somarNum(num1, num2){
+    return num1 + num2
+}
+
+button.addEventListener('click', ())
